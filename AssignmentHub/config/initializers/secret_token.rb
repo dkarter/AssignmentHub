5786831e047ecd51adab525a057c21490caf626d
Joseph Low <nephanim@gmail.com>
2012-02-22 20:42:11 -0600

    Fix CRLF?

--- AssignmentHub/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-AssignmentHub::Application.config.secret_token = '436b04be81e7857819b800b99572bbe48de1ee3d45089af2684dbbb148d41293ad9ab4398d07b18a981ab322182d18847d62d74ff87c46ce3105eb7051be1a48'
