03181532b5d1a06df42873e9832dd82ce0567500
Joseph Low <nephanim@gmail.com>
2012-02-23 10:12:13 -0600

    Validation of fields for new users

--- AssignmentHub/app/models/user.rb
+++ AssignmentHub/app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  # Validation of various parameters. Important: require valid email format, unique username
+  validates_length_of :name, :within => 3..15
+  validates_length_of :password, :within => 5..40
+  validates_presence_of :name, :password, :first, :last, :email, :user_type
+  validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i, :message => "Invalid email"
+  validates_uniqueness_of :name
 end
