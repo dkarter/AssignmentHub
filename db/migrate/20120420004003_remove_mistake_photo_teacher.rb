620a2f2dce40952c723ea0de2448433883bf1583
Amar Rana <arana7@gmail.com>
2012-04-20 12:41:29 -0500

    schema fixed w/out mistake migration

--- db/migrate/20120420004003_remove_mistake_photo_teacher.rb
+++
@@ -1,7 +0,0 @@
-class RemoveMistakePhotoTeacher < ActiveRecord::Migration
-  def change 
-    remove_column :teachers, :photo_file_name
-    remove_column :teachers, :photo_content_type
-    remove_column :teachers, :photo_file_size
-  end
-end
