78c5ccf36846466364637e1afd66631d3c0a017f
Amar Rana <arana7@gmail.com>
2012-04-20 14:17:35 -0500

    validations w/out university

--- app/models/university.rb
+++ app/models/university.rb
@@ -1,4 +1,3 @@
 class University < ActiveRecord::Base
-  validates_presence_of :name, :city, :state, :website, :message => "must provide basic information"
-  validates_length_of :name, :city, :state, :maximum => 15, :message => "less than 15 characters please"
+
 end
