a123b80395677cd3e7094e1bcf72cc76b041e703
Nephanim <nephanim@gmail.com>
2012-04-22 21:53:36 -0500

    Before create assignment, schedule notification

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,11 +1,14 @@
 class Assignment < ActiveRecord::Base
   belongs_to :course
+
+  before_create :schedule_notification
   
   validates_presence_of :name, :message => "must provide basic information"
   validates_length_of :name, :maximum => 15, :message => "less than 15 characters please"
 
   def schedule_notification()
-    @assignment = self
+    name = self.name
+    due = self.due_date - 1.day
     #course = self.course
     #user = course.user.name
     #user = User.new(:name => "Bob", :password => "password", :first => "Bob", :last => "Bobber", :email => "nephanim@gmail.com")
@@ -15,7 +18,7 @@ class Assignment < ActiveRecord::Base
     #course = "Test Course"
     #due = "tomorrow"
     #Notifications.deliver_notification(email, name, course, assignment, due)
-    Notifications.deliver_notification("nephanim@gmail.com", "Nephanim", "Test Course", "Test Assignment", "tomorrow").deliver
+    Notifications.deliver_notification("nephanim@gmail.com", "Nephanim", "Test Course", name, due).deliver
   end
 
 end
